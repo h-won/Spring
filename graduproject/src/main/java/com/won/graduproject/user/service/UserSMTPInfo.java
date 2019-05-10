8890b5f0f393a54bd01f4b8d2376b6d09a2c078a
h-won <40622229+h-won@users.noreply.github.com>
2019-05-10 07:08:48 +0000

    Delete UserSMTPInfo.java

--- graduproject/src/main/java/com/won/graduproject/user/service/UserSMTPInfo.java
+++
@@ -1,8 +0,0 @@
-package com.won.graduproject.user.service;
-
-public class UserSMTPInfo {
-	public static final String hostSMTPid = "wow642";
-	public static final String hostSMTPpwd = "dong0125";
-	public static final String fromEmail = "wow642@naver.com";
-	public static final String redirect_url = "http://210.123.254.74:8080/graduproject/user/approval_user.do";
-}
